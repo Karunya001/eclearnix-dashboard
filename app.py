ca74bbf96db7f90977e793d34ac0fb8e51be4a96
KARUNYA <karunyas.22cse@kongu.edu>
2025-08-02 14:48:17 +0530

    Fix Render port binding issue

--- app.py
+++ app.py
@@ -160,5 +160,6 @@ def render_content(tab):
 
 # ---------- Run App ----------
 if __name__ == '__main__':
-    app.run(debug=True)
-s
+    import os
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port, debug=False)
